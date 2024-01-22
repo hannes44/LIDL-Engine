0510f4f8872cb9c56e8f9909e4d180f6c7a42a03
jwindt <jakob.windt@gmail.com>
2024-01-22 13:42:32 +0100

    Switched to library uuid

--- engine/main.cpp
+++ engine/main.cpp
@@ -2,15 +2,24 @@
 #include "transform.hpp"
 #include "TestGame.hpp"
 #include "logger.hpp"
+#include "uuid_v4.h"
 
 int main(int argc, char* argv[])
 {
 	engine::Logger::init();
 
+	/*
 	auto game = engine::TestGame();
 	game.initialize();
 	game.gameLoop();
-	
+	*/
+
+	UUIDv4::UUIDGenerator<std::mt19937_64> uuidGenerator;
+	UUIDv4::UUID uuid = uuidGenerator.getUUID();
+	std::string s = uuid.str();
+
+	std::cout << s;
+
 	return 0;
 }
 
