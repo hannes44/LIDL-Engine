18a60b9ab229c404b88d6911837b6d9059f58a61
Hannes Kaulio <hannes.kaulio@gmail.com>
2024-01-22 11:40:35 +0100

    Added logger

--- engine/main.cpp
+++ engine/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "transform.hpp"
 #include "TestGame.hpp"
+#include "logger.hpp"
 
 int main(int argc, char* argv[])
 {
+	engine::Logger::init();
+
 	auto game = engine::TestGame();
 	game.initialize();
 	game.gameLoop();
