9cf6d241f8c0c61c5ea3538acb93090e2312da90
Hannes Kaulio <hannes.kaulio@gmail.com>
2024-01-22 18:43:43 +0100

    Changed file names

--- engine/Vertex.hpp
+++ engine/Vertex.hpp
@@ -6,9 +6,9 @@ namespace engine
 {
 	struct Vertex
 	{
-		glm::vec3 position;
-		glm::vec3 color;
-		glm::vec3 normal;
-		glm::vec2 texCoords;
+		glm::vec3 position{};
+		glm::vec3 color{};
+		glm::vec3 normal{};
+		glm::vec2 texCoords{};
 	};
 }
