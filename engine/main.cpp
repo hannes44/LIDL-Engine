01a527e98a6baeb5c8a1001bfc13eade4a82d16d
jwindt <jakob.windt@gmail.com>
2024-01-23 11:58:59 +0100

    Added glew-cmake

--- engine/main.cpp
+++ engine/main.cpp
@@ -2,15 +2,20 @@
 #include "transform.hpp"
 #include "TestGame.hpp"
 #include "logger.hpp"
+#include "GL/glew.h"
 
 int main(int argc, char* argv[])
 {
 	engine::Logger::init();
 
+	/*
 	auto game = engine::TestGame();
 	game.initialize();
 	game.gameLoop();
-	
+	*/
+
+	glewInit();
+
 	return 0;
 }
 
