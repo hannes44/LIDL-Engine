864e0c77c43e175d936f7aa879daf7bcea9a9fda
Hannes Kaulio <hannes.kaulio@gmail.com>
2024-02-12 23:00:16 +0100

    Created template component script

---
+++ Engine/src/ScriptingAPI/ComponentTemplate.cs
@@ -0,0 +1,23 @@
+using Engine;
+using System;
+
+public class TemplateComponent
+{
+    // The unique identifier for this component
+    public string Id;
+    // The gameObject this component is attached to
+    public GameObject gameObject = new GameObject();
+
+
+    // Update is called once per frame
+    public void Update()
+    {
+
+    }
+
+    // Initialize is called once the component is added to the game
+    public void Initialize()
+    {
+
+    }
+}
