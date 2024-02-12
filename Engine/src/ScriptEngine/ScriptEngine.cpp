49a842a7abb741f8b715c819257c75bb72575044
Hannes Kaulio <hannes.kaulio@gmail.com>
2024-02-13 00:10:25 +0100

    Moved function to utils

--- Engine/src/ScriptEngine/ScriptEngine.cpp
+++ Engine/src/ScriptEngine/ScriptEngine.cpp
@@ -9,6 +9,7 @@
 #include<algorithm>
 #include <windows.h>
 #include <atlstr.h>
+#include "Utils/Utils.hpp"
 
 namespace engine
 {
@@ -59,78 +60,6 @@ namespace engine
 		return instance;
 	}
 
-
-	// https://stackoverflow.com/questions/478898/how-do-i-execute-a-command-and-get-the-output-of-the-command-within-c-using-po
-	//
-	// Execute a command and get the results. (Only standard output)
-	//
-	CStringA ExecCmd(
-		const wchar_t* cmd              // [in] command to execute
-	)
-	{
-		CStringA strResult;
-		HANDLE hPipeRead, hPipeWrite;
-
-		SECURITY_ATTRIBUTES saAttr = { sizeof(SECURITY_ATTRIBUTES) };
-		saAttr.bInheritHandle = TRUE; // Pipe handles are inherited by child process.
-		saAttr.lpSecurityDescriptor = NULL;
-
-		// Create a pipe to get results from child's stdout.
-		if (!CreatePipe(&hPipeRead, &hPipeWrite, &saAttr, 0))
-			return strResult;
-
-		STARTUPINFOW si = { sizeof(STARTUPINFOW) };
-		si.dwFlags = STARTF_USESHOWWINDOW | STARTF_USESTDHANDLES;
-		si.hStdOutput = hPipeWrite;
-		si.hStdError = hPipeWrite;
-		si.wShowWindow = SW_HIDE; // Prevents cmd window from flashing.
-		// Requires STARTF_USESHOWWINDOW in dwFlags.
-
-		PROCESS_INFORMATION pi = { 0 };
-
-		BOOL fSuccess = CreateProcessW(NULL, (LPWSTR)cmd, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi);
-		if (!fSuccess)
-		{
-			CloseHandle(hPipeWrite);
-			CloseHandle(hPipeRead);
-			return strResult;
-		}
-
-		bool bProcessEnded = false;
-		for (; !bProcessEnded;)
-		{
-			// Give some timeslice (50 ms), so we won't waste 100% CPU.
-			bProcessEnded = WaitForSingleObject(pi.hProcess, 50) == WAIT_OBJECT_0;
-
-			// Even if process exited - we continue reading, if
-			// there is some data available over pipe.
-			for (;;)
-			{
-				char buf[1024];
-				DWORD dwRead = 0;
-				DWORD dwAvail = 0;
-
-				if (!::PeekNamedPipe(hPipeRead, NULL, 0, NULL, &dwAvail, NULL))
-					break;
-
-				if (!dwAvail) // No data available, return
-					break;
-
-				if (!::ReadFile(hPipeRead, buf, min(sizeof(buf) - 1, dwAvail), &dwRead, NULL) || !dwRead)
-					// Error, the child process might ended
-					break;
-
-				buf[dwRead] = 0;
-				strResult += buf;
-			}
-		} //for
-
-		CloseHandle(hPipeWrite);
-		CloseHandle(hPipeRead);
-		CloseHandle(pi.hProcess);
-		CloseHandle(pi.hThread);
-		return strResult;
-	} //ExecCmd
 	
 
 	void ScriptEngine::initializeLuaStateForScriptableComponent(ScriptableComponent* component)
@@ -253,14 +182,13 @@ namespace engine
 		LOG_INFO("Starting script engine");
 
 		LOG_INFO("Compiling C# scripts to lua");
-		// Compiling the C# scripts to Lua
-		// 
-		// 
+
 		std::string compileCommand = "dotnet ../../engine/src/ScriptingAPI/C#ToLuaCompiler/CSharp.Lua.Launcher.dll -s ../../Games/TestGame/Scripts -d ../../Games/TestGame/Scripts/Compiled";
 		std::wstring widestr = std::wstring(compileCommand.begin(), compileCommand.end());
 		const wchar_t* widecstr = widestr.c_str();
 		
-		CStringA result = ExecCmd(widecstr);
+		CStringA result = Utils::ExecCmd(widecstr);
+
 		std::string resultString = result.GetString();
 		std::string errorPattern = "error";
 		if (resultString.find(errorPattern) != std::string::npos) {
