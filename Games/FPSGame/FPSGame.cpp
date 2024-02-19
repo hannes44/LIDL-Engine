9310d60810294047cfa38ae432ee45396b96551b
Hannes Kaulio <hannes.kaulio@gmail.com>
2024-02-19 15:32:18 +0100

    Added FPSGame

---
+++ Games/FPSGame/FPSGame.cpp
@@ -0,0 +1,40 @@
+#include "FPSGame.hpp"
+#include <iostream>
+
+extern "C" {
+	__declspec(dllexport) engine::Game* createGame(engine::Renderer* renderer, engine::Window* window, engine::InputFramework* inputFramework, engine::ResourceManager* resourceManager) {
+		engine::Game* game = new engine::FPSGame();
+		engine::Renderer::instance = renderer;
+		engine::Window::instance = window; 
+		engine::InputFramework::instance = inputFramework;
+		engine::Logger::init();
+		engine::ResourceManager::instance = resourceManager;
+		resourceManager->changeGame(game);
+		return game;
+	}
+}
+namespace engine {
+	double FPSGame::getTargetFrameRate() {
+		return 10;
+	}
+
+	FPSGame::FPSGame()
+	{
+		name = "FPSGame";
+		glewInit();
+	}
+
+	void FPSGame::update() {
+
+	}
+
+	void FPSGame::initialize() {
+
+	}
+
+}
+
+engine::Game* engine::createGame() {
+
+	return new FPSGame();
+}
