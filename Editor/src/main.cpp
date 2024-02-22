dc97756d6670ac52b4cb3918afdcd37fea4cc019
marc7s <marcus.schagerberg@gmail.com>
2024-02-22 18:21:22 +0100

    Added headers and message types to client

--- Editor/src/main.cpp
+++ Editor/src/main.cpp
@@ -1,13 +1,19 @@
 #include "Editor.hpp"
 #include "Multiplayer/Client/Client.hpp"
+#include <stdio.h>
 
 void runEditor() {
 	engine::Editor editor{};
 	editor.start();
 }
 
+void onMessage(std::string msg) {
+	std::cout << "New Message:" << std::endl;
+	std::cout << msg << std::endl;
+}
+
 void runMultiplayer() {
-	engine::Client::run();
+	engine::Client::run(onMessage);
 }
 
 int main(int argc, char* argv[]) {
--- Engine/src/Multiplayer/Client/Client.hpp
+++ Engine/src/Multiplayer/Client/Client.hpp
@@ -1,16 +1,24 @@
 #include <winsock2.h>
 #include <string>
+#include <functional>
 
 namespace engine {
+	enum ClientMessageType {
+		StateUpdate,
+		CustomMessage
+	};
+
 	class Client {
 	public:
-		static int run();
+		static int run(std::function<void(std::string)> onStateUpdate);
 
 	private:
+		static std::string GetHeader(ClientMessageType type);
 		static bool SocketSend(SOCKET socket, std::string message);
-		static bool SendMsg(SOCKET socket, std::string message);
+		static bool SendMsg(SOCKET socket, std::string message, ClientMessageType type);
 		static std::string ReceiveMsg(SOCKET socket);
-		static std::string SendAndReceiveMsg(SOCKET socket, std::string message);
+		static std::string SendAndReceiveMsg(SOCKET socket, std::string message, ClientMessageType type);
+		static std::string CleanChunk(std::string chunk);
 		static std::string CleanMsg(std::string message);
 		static std::string GetGameState(const std::string& gameName);
 	};
