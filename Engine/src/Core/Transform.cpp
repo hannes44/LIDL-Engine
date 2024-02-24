140c930e384057151fec96c8bf4147c972c70a59
jwindt <jakob.windt@gmail.com>
2024-02-24 12:27:26 +0100

    Improved rotations

--- Engine/src/Core/Transform.cpp
+++ Engine/src/Core/Transform.cpp
@@ -16,18 +16,38 @@ void engine::Transform::setPosition(glm::vec3 position)
 	transformMatrix[3] = glm::vec4(position, 1.0f);
 }
 
-glm::quat engine::Transform::getRotation() {
+glm::quat engine::Transform::getRotation() 
+{
 	return glm::quat(transformMatrix);
 }
 
 // TODO: Fix this, I do not trust it at all
-void engine::Transform::setRotation(glm::quat rotation) {
-	transformMatrix = glm::rotate(transformMatrix, glm::angle(rotation) - glm::angle(getRotation()), glm::vec3(0, 1, 0));
+void engine::Transform::setRotation(glm::quat rotation) 
+{
+	//transformMatrix = glm::rotate(transformMatrix, glm::angle(rotation) - glm::angle(getRotation()), glm::vec3(0, 1, 0));
 }
 
 // TODO: Fix this, I do not trust it at all
-void engine::Transform::setRotationFromDirection(glm::vec3 direction) {
-	setRotation(glm::quatLookAt(glm::normalize(direction), glm::vec3(0, 1, 0)));
+void engine::Transform::setRotationFromDirection(glm::vec3 direction, glm::vec3 normal) 
+{
+	glm::vec3 up = normal;
+
+	glm::vec3 xaxis = glm::normalize(glm::cross(up, direction));
+
+	glm::vec3 yaxis = glm::normalize(glm::cross(direction, xaxis));
+
+	transformMatrix[0][0] = xaxis.x;
+	transformMatrix[0][1] = yaxis.x;
+	transformMatrix[0][2] = direction.x;
+
+	transformMatrix[1][0] = xaxis.y;
+	transformMatrix[1][1] = yaxis.y;
+	transformMatrix[1][2] = direction.y;
+
+	transformMatrix[2][0] = xaxis.z;
+	transformMatrix[2][1] = yaxis.z;
+	transformMatrix[2][2] = direction.z;
+
 }
 
 void engine::Transform::shiftPosition(glm::vec3 offset)
