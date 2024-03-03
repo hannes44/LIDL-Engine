7c74e3ca757ecf9e7854b3de4b7cc0e5157ddf60
Hannes Kaulio <hannes.kaulio@gmail.com>
2024-03-03 13:11:38 +0100

    Added temporary crosshair

--- Editor/src/GUI/EditorGUI.cpp
+++ Editor/src/GUI/EditorGUI.cpp
@@ -173,6 +173,24 @@ namespace engine
 			drawTopMenu();
 			drawPlayButtonToolbar();
 			drawBottomPanel();
+			
+			// Temporary code until game UI system is created
+			// Crosshair for the game
+			ImGuiWindowFlags windowFlags = 0;
+			windowFlags |= ImGuiWindowFlags_NoBackground;
+			windowFlags |= ImGuiWindowFlags_NoTitleBar;
+			windowFlags |= ImGuiWindowFlags_NoMove;
+			windowFlags |= ImGuiWindowFlags_NoResize;
+			windowFlags |= ImGuiWindowFlags_NoScrollbar;
+			windowFlags |= ImGuiWindowFlags_NoScrollWithMouse;
+			windowFlags |= ImGuiWindowFlags_NoCollapse;
+			windowFlags |= ImGuiWindowFlags_NoBringToFrontOnFocus;
+			ImGui::Begin("#CH", nullptr, windowFlags);
+			auto draw = ImGui::GetBackgroundDrawList();
+			int w, h;
+			window.getWindowSize(&w, &h);
+			draw->AddCircle(ImVec2(w / 2, h / 2), 6, IM_COL32(255, 0, 0, 255), 100, 0.0f);
+			ImGui::End();
 
 			if (!ScriptEngine::getInstance()->isSuccessfullyCompiled)
 			{
